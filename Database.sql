6b6d3747141e9c824f8ee67732091765a676bc5b
Cameron-Bone <37344006+Cameron-Bone@users.noreply.github.com>
2019-02-07 13:13:56 +0000

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `Campaign` (
-  `campaignID` int(6) NOT NULL UNIQUE,
-  `clientID` int(6) NOT NULL UNIQUE,
+  `campaignID` int(6) NOT NULL AUTO_INCREMENT,
+  `clientID` int(6) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY(campaignID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `Advert` (
-  `advertID` int(6) NOT NULL UNIQUE,
-  `campaignID` int(6) NOT NULL UNIQUE,
+  `advertID` int(6) NOT NULL AUTO_INCREMENT,
+  `campaignID` int(6) NOT NULL AUTO_INCREMENT,
   `type` varchar(8) NOT NULL,
   PRIMARY KEY (advertID),
   FOREIGN KEY (campaignID) REFERENCES Campaign(campaignID)
@@ -15,22 +15,22 @@ CREATE TABLE `Advert` (
 
 
 CREATE TABLE `Client` (
-  `clientID` int(6) NOT NULL UNIQUE,
+  `clientID` int(6) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY(clientID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `Invoice` (
-  `invoiceID` int(6) NOT NULL UNIQUE,
-  `campaignID` int(6) NOT NULL UNIQUE,
-  `clientID` int(6) NOT NULL UNIQUE,
+  `invoiceID` int(6) NOT NULL AUTO_INCREMENT,
+  `campaignID` int(6) NOT NULL AUTO_INCREMENT,
+  `clientID` int(6) NOT NULL AUTO_INCREMENT,
   `cost` int(16) NOT NULL,
   PRIMARY KEY(invoiceID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `Magazine` (
-  `advertID` int(6) NOT NULL UNIQUE,
+  `advertID` int(6) NOT NULL AUTO_INCREMENT,
   `magazine` varchar(32) NOT NULL,
   `size` varchar(16) NOT NULL,
   `position` varchar(16) NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE `Magazine` (
 
 
 CREATE TABLE `TV&Radio` (
-  `advertID` int(6) NOT NULL UNIQUE,
+  `advertID` int(6) NOT NULL AUTO_INCREMENT,
   `slot` varchar(32) NOT NULL,
   `length` int(5) NOT NULL,
   `station` varchar(32) NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE `TV&Radio` (
 
 
 CREATE TABLE `Web` (
-  `advertID` int(6) NOT NULL UNIQUE,
+  `advertID` int(6) NOT NULL AUTO_INCREMENT,
   `company` varchar(64) NOT NULL,
   `demographic` varchar(32) NOT NULL,
   `region` varchar(64) NOT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE `Web` (
 
 
 CREATE TABLE `WorkDone` (
-  `campaignID` int(6) NOT NULL UNIQUE,
-  `employeeID` int(6) NOT NULL UNIQUE,
+  `campaignID` int(6) NOT NULL AUTO_INCREMENT,
+  `employeeID` int(6) NOT NULL AUTO_INCREMENT,
   `hoursWorked` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
