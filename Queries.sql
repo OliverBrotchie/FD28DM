a0ea17534324be022ea65c36d27c1d75b46f16cc
Cameron-Bone <37344006+Cameron-Bone@users.noreply.github.com>
2019-02-13 16:23:43 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,4 @@
-/* Query 1 (not working) */
+/* Query 1 (Working(ish)) */
 SELECT DISTINCT Employee.employeeID, PersonalInfo.firstName, PersonalInfo.secondName
 FROM PersonalInfo
 	INNER JOIN Employee
@@ -9,7 +9,18 @@ FROM PersonalInfo
 		ON Campaign.campaignID = WorkDone.campaignID
 	INNER JOIN Advert
 		ON Advert.campaignID = Campaign.campaignID
-WHERE Advert.form != 'radio';
+WHERE Employee.employeeID NOT IN (
+SELECT DISTINCT Employee.employeeID
+FROM PersonalInfo
+	INNER JOIN Employee
+		ON Employee.personID = PersonalInfo.personID
+	INNER JOIN WorkDone
+		ON WorkDone.employeeID = Employee.employeeID
+	INNER JOIN Campaign
+		ON Campaign.campaignID = WorkDone.campaignID
+	INNER JOIN Advert
+		ON Advert.campaignID = Campaign.campaignID
+	WHERE Advert.form = 'radio');
 
 
 
